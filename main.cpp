8f5a75ce90f892236c77126c2447ceed9d0ab420
Austin Nadler <austinnadler706@gmail.com>
2019-02-25 19:32:16 -0600

    Program now generates input files for InventorySystem / POS System

--- main.cpp
+++ main.cpp
@@ -3,20 +3,126 @@
 #include <string>
 #include <string>
 #include <iostream>
+#include <sstream>
+#include <vector>
+#include <stdexcept>
+#include <fstream>
 using namespace std;
+
 string random_string(string::size_type length);
 string random_stringofint(string::size_type length);
+bool codeAv(string& code);
+bool openFileOut(ofstream& ofs, const string& fileName);
+
+
+vector<string> codes; // codes must be unique
+
 int main() {
+    const string filename = "items.csv";
+    string name, price, numOnHand, code, warning, age;
+    ostringstream oss;
+    ofstream ofs;
+    const int BOUND = 500;
+    int length;
+    bool validCode = false;
+    bool validAge = false;
+    bool validNumOnHand = false;
+    bool validPrice = false;
+    for(int i = 0; i < BOUND; i++) {
+        length = rand() % 20 + 5;
+        name = random_string(length);
+        length = rand() % 4 + 1;
+        price = random_stringofint(length);
+        length = rand() % 4 + 1;
+        numOnHand = random_stringofint(length);
+        do {
+            code = random_stringofint(5);
+            validCode = codeAv(code);
+        } while(!validCode);
+        if(validCode) {
+            codes.push_back(code);
+        }
+        oss << "gm," << name << "," << price << "," << numOnHand << "," << code << endl;
+    }
+    
+    for(int i = 0; i < BOUND; i++) {
+        validAge = false;
 
-    string genStr = random_string(16);
-    cout << genStr << endl;
+        length = rand() % 20 + 5;
+        name = random_string(length);
 
-    string genInt = random_stringofint(2);
-    cout << genInt << endl;
+        length = rand() % 4 + 1;
+        do {
+            try {
+                price = random_stringofint(length);
+                if(stoi(price) > 0) {
+                    validPrice = true;
+                }
+            } catch(invalid_argument e) {
+                validPrice = false;
+            }
+            
+        } while(!validPrice);
+        
+        length = rand() % 4 + 1;
+        numOnHand = random_stringofint(length);
 
+        length = rand() % 21;
+        warning = random_string(length);
+
+        length = rand() % 2 + 1;
+        age = random_stringofint(length);
+        do {
+            code = random_stringofint(5);
+            validCode = codeAv(code);
+        } while(!validCode);
+        if(validCode) {
+            codes.push_back(code);
+        }
+        oss << "ar," << age << "," << name << "," << price << "," << numOnHand << "," << code << endl;
+    }
+
+    for(int i = 0; i < BOUND; i++) {
+        validAge = false;
+
+        length = rand() % 20 + 5;
+        name = random_string(length);
+
+        length = rand() % 4 + 1;
+        do {
+            try {
+                price = random_stringofint(length);
+                if(stoi(price) > 0) {
+                    validPrice = true;
+                }
+            } catch(invalid_argument e) {
+                validPrice = false;
+            }
+            
+        } while(!validPrice);
+        
+        length = rand() % 4 + 1;
+        numOnHand = random_stringofint(length);
+
+        length = rand() % 21;
+        warning = random_string(length);
+
+        length = rand() % 20 + 1;
+        warning = random_string(length);
+        do {
+            code = random_stringofint(5);
+            validCode = codeAv(code);
+        } while(!validCode);
+        if(validCode) {
+            codes.push_back(code);
+        }
+        oss << "warn," << warning << "," << name << "," << price << "," << numOnHand << "," << code << endl;
+    }    
+    bool fileStatus = openFileOut(ofs, filename);
+    ofs << oss.str();
     return 0;
 }// end main()
-using namespace std;
+
 string random_string(string::size_type length) {
     static auto& chrs =
         "abcdefghijklmnopqrstuvwxyz"
@@ -37,7 +143,7 @@ string random_string(string::size_type length) {
 
 string random_stringofint(string::size_type length) {
     static auto& chrs =
-        "1234567890";
+        "123456789";
 
     thread_local static mt19937 rg{random_device{}()};
     thread_local static uniform_int_distribution<string::size_type> pick(0, sizeof(chrs) - 2);
@@ -52,3 +158,20 @@ string random_stringofint(string::size_type length) {
     return s;
 }
 
+bool codeAv(string& code) {
+    if(codes.size() == 0) {
+        return true;
+    } else {
+        for(int i = 0; i < codes.size(); i++) {
+            if(code == codes.at(i)) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+bool openFileOut(ofstream& ofs, const string& fileName){
+    ofs.open(fileName);
+    return ofs.is_open();
+}// end openFileOut()
